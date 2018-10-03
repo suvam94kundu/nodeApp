724240e7318282d53fd748bfb9ab4b6270952a81
suvam94kundu <svmkundu1993@gmail.com>
2018-10-03 18:43:03 +0530

    app.js updated

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var app = express();
 var mongo = require('mongoose');
 var config = require('./config/index');
 var setUpController = require('./controllers/seedDbController');
+var apiController = require("./controllers/apiController");
 
 app.set('view engine', 'ejs');
-var port = 8085;
+var port = 9085;
 mongo.connect(config.getDbConnections(), { useNewUrlParser: true }).then(
     () => { console.log('connected') },
     err => { console.log(err) }
-);;
+);
 setUpController(app);
+apiController(app);
 var server = app.listen(port, function(err) {
     var host = server.address().address;
     var port = server.address().port;
